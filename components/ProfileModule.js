4d5e2f77775e8388450031337bdd9609db360341
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-25 17:07:17 +0200

    feat: welcome message component

---
+++ components/ProfileModule.js
@@ -0,0 +1,19 @@
+
+
+Vue.component('profile-module', {
+    template: 
+`
+<div id="profile-module" class="card testimonial-card mt-5 pt-5">
+	<div class="avatar mx-auto white p-5"><img src="./images/sebastian.png" alt="avatar mx-auto white"
+		class="rounded-circle img-fluid z-depth-2">
+    </div>
+	<div class="card-body">
+		<h4 class="card-title mt-1"><a href="https://github.com/sebastian-porling">Sebastian Porling</a></h4>
+		<hr>
+		<p><i class="fas fa-quote-left"></i>
+			Hallo! Bitteschön, ein in Vue.js geschriebener Währungsumrechner!
+		</p>
+	</div>
+</div>
+`
+});
